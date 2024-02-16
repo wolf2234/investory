11553ea59ec6eb95e4ce5415067dfb90ddaa6c85
wolf2234 <viktoral9599@gmail.com>
2024-02-16 16:46:38 +0200

    this commit creates the calcualte in the Investors.

--- js/script.js
+++ js/script.js
@@ -11,7 +11,6 @@ $(document).ready(function() {
         event.preventDefault();
         sidebarBtn.toggleClass('left-sidebar-menu__active');
     });
-});
 
 $('.direction-blocks').slick({
     arrows: false,
@@ -36,5 +35,38 @@ $('.direction-blocks').slick({
     ]
 });
 
+var elem = document.querySelector('.calc-range');
+var init = new Powerange(elem, { min: 100000, 
+                                max: 3000000, 
+                                start: 100000, 
+                                hideRange: true,
+                                step: 100000 });
+var per, month, result, total, monthly;
+var money = +$('.calc-range').val();
 
 
+$('input[name="programms"]').on('change', function(event){
+    month = +$(this).attr('data-month');
+    per = +$(this).attr('data-per');
+    result = Math.round(per / 12 * month * money);
+    total = result + money;
+    monthly = parseInt(result / month);
+    $('#total').text(total.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+    $('.calc-monthly span').text(monthly.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+    $('#month').text(month);
+});
+
+$('.calc-range').on('change', function(event) {
+    $('.calc-summ-invest-num span').text($(this).val().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+    var radio = $('input[name="programms"]:checked');
+    money = +$(this).val();
+    month = +radio.attr('data-month');
+    per = +radio.attr('data-per');
+    result = Math.round(per / 12 * month * money);
+    total = result + money;
+    monthly = parseInt(result / month);
+    $('#total').text(total.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+    $('.calc-monthly span').text(monthly.toString().replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 '));
+});
+
+});
